03d1ccdedb5623ee6e93028917dfbb8d72723ff2
johnburnmurdoch <j.burn.murdoch@gmail.com>
2016-09-15 12:58:37 +0100

    Re-ordererd

--- slides/r-ggplot/r-scripts.R
+++ slides/r-ggplot/r-scripts.R
@@ -1,41 +1,43 @@
+allSeasons <- read.csv('https://raw.githubusercontent.com/johnburnmurdoch/johnburnmurdoch.github.io/master/slides/r-ggplot/changing-tides-of-football.csv', stringsAsFactors = F)
+oneSeason <- allSeasons %>% filter(year==2016)
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(magrittr)
+library(RColorBrewer)
+library(scales)
+
 #1
-ggplot(data %>% 
-         filter(year==2016)
+ggplot(oneSeason
        ,aes(x=1,score,col=country)) + 
   geom_point(size=3) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  facet_wrap(~year, ncol=4) + 
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
   theme(legend.position="right", legend.direction="vertical") +
   xlab('')
 
 #2
-ggplot(data %>% 
-         filter(year==2016)
+ggplot(oneSeason
        ,aes(rank,score,col=country)) + 
   geom_point(size=3) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  facet_wrap(~year, ncol=4) + 
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
-  theme(legend.position="none", legend.direction="vertical") +
-  xlab('')
+  theme(legend.position="top")
 
 #3
-ggplot(data %>% 
-         filter(year==2016)
+ggplot(oneSeason
        ,aes(rank,score,col=country)) + 
   geom_path() + 
   geom_point(size=3) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  facet_wrap(~year, ncol=4) + 
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
-  theme(legend.position="top") +
-  xlab('')
+  theme(legend.position="top")
 
 #4
-ggplot(data %>%
+ggplot(allSeasons %>%
          filter(year==2016 & country %in% c('ENG','ESP')) %>%
-         dplyr::select(score, year, rank, country) %>% 
+         dplyr::select(score, rank, country) %>% 
          spread(., country, score) %>% 
          rowwise() %>%
          mutate(
@@ -45,52 +47,52 @@ ggplot(data %>%
          )
        ,aes(rank,min,fill=gap>0)) + 
   geom_rect(aes(xmin = rank-0.5, xmax=rank+0.5, ymin=min, ymax=max), alpha=0.5) +
-  scale_fill_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_fill_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
-  theme(legend.position="top")
+  theme(legend.position="none")
 
 #5
-ggplot(data %>%
-         filter(year==2016 & country == 'ENG'),aes(rank,score,col=country)) + 
+ggplot(oneSeason %>%
+         filter(country == 'ENG'),aes(rank,score,col=country)) + 
   geom_ribbon(aes(ymin = atw, ymax = atb, fill=country),col='transparent', alpha=0.5) +
   geom_path(alpha=1, size=1.5) + 
   geom_point(size=3) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  scale_fill_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
+  scale_fill_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
   theme(legend.position="none")
 
 #6
-ggplot(data %>%
-         filter(year==2016 & country == 'ENG'),aes(rank,score,col=country, fill=country)) + 
+ggplot(oneSeason %>%
+         filter(country == 'ENG'),aes(rank,score,col=country, fill=country)) + 
   geom_rect(aes(xmin=rank-0.5, xmax=rank+0.5, ymin = atw, ymax = atb),col='transparent', alpha=0.5) +
   geom_point(size=5) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  scale_fill_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
+  scale_fill_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
   theme(legend.position="none")
 
 #7
-ggplot(data %>%
-         filter(year==2016 & country == 'ENG'),aes(rank,score,col=country, fill=country)) + 
+ggplot(oneSeason %>%
+         filter(country == 'ENG'),aes(rank,score,col=country, fill=country)) + 
   geom_rect(aes(xmin=rank-0.45, xmax=rank+0.45, ymin = atw, ymax = atb),col='transparent', alpha=0.5) +
   geom_point(size=5) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  scale_fill_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
+  scale_fill_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   theme_bw() +
   theme(legend.position="none")
 
 #8
-ggplot(data,aes(rank,score,col=country)) + 
+ggplot(allSeasons,aes(rank,score,col=country)) + 
   geom_path() + 
   geom_point(size=1) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   facet_wrap(~year, ncol=4) + 
   theme_bw() +
   theme(legend.position="top")
 
 #9
-ggplot(data %>%
+ggplot(allSeasons %>%
          filter(country %in% c('ENG','ESP')) %>%
          dplyr::select(score, year, rank, country) %>% 
          spread(., country, score) %>% 
@@ -102,18 +104,18 @@ ggplot(data %>%
          )
        ,aes(rank,min,fill=gap>0)) + 
   geom_rect(aes(xmin = rank-0.5, xmax=rank+0.5, ymin=min, ymax=max), alpha=0.5) +
-  scale_fill_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_fill_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   facet_wrap(~year, ncol=4) + 
   theme_bw() +
   theme(legend.position="none")
 
 #10
-ggplot(data %>% filter(country == 'ENG'),aes(rank,score,col=country, fill=country)) + 
+ggplot(allSeasons %>% filter(country == 'ENG'),aes(rank,score,col=country, fill=country)) + 
   geom_rect(aes(xmin=rank-0.45, xmax=rank+0.45, ymin = atw, ymax = atb),col='transparent', alpha=0.5) +
   geom_path(alpha=1) + 
   geom_point(size=1) + 
-  scale_color_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
-  scale_fill_manual(breaks=unique(data$country),values=brewer_pal('qual',2)(7)) +
+  scale_color_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
+  scale_fill_manual(breaks=unique(allSeasons$country),values=brewer_pal('qual',2)(7)) +
   facet_wrap(~year, ncol=4) + 
   theme_bw() +
   theme(legend.position="none")
